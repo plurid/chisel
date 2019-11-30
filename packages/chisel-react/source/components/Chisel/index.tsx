c875205192dafcd6de0c1cd83e5ec0367e3edc1f
Jan Cavel <caveljan@gmail.com>
2019-11-30 16:14:13 +0200

    chisel-react > dev: Chisel

--- packages/chisel-react/source/components/Chisel/index.tsx
+++ packages/chisel-react/source/components/Chisel/index.tsx
@@ -25,9 +25,17 @@ const Chisel: React.FC<ChiselProperties> = (properties) => {
         nodes,
     } = value;
 
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+        console.log(event);
+    }
+
     return (
         <StyledChisel
             theme={theme}
+            onKeyDown={handleKeyDown}
+            tabIndex={0}
+            contentEditable={true}
+            suppressContentEditableWarning={true}
         >
             {nodes.map(node => {
                 const {
--- packages/chisel-react/source/components/Chisel/styled.ts
+++ packages/chisel-react/source/components/Chisel/styled.ts
@@ -5,7 +5,9 @@ import styled from 'styled-components';
 export const StyledChisel = styled.div`
     background-color: ${props => props.theme.backgroundColorPrimary};
     color: ${props => props.theme.colorPrimary};
+
     font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
     font-size: 0.9rem;
     padding: 1rem;
+    outline: none;
 `;
