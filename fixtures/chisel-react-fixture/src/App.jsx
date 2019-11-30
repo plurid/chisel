44649bbb8522b5c74c8d41b45372d5395fc94c20
Jan Cavel <caveljan@gmail.com>
2019-11-30 14:38:34 +0200

    chisel-react > fixture: package

---
+++ fixtures/chisel-react-fixture/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Chisel, {
+    ChiselValue,
+} from '@plurid/chisel-react';
+
+
+
+const value: ChiselValue = {
+    nodes: [
+        {
+            text: 'one',
+        },
+        {
+            text: 'two',
+        },
+    ],
+};
+
+const App = () => {
+    return (
+        <div>
+            <Chisel
+                value={value}
+            />
+        </div>
+    );
+}
+
+
+export default App;
