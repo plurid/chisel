14812fbca8344a5090f60fe98c8f3e94e9eb92bf
Jan Cavel <caveljan@gmail.com>
2019-11-30 15:34:45 +0200

    chisel-react > setup: package

--- packages/chisel-react/source/interfaces/index.ts
+++ packages/chisel-react/source/interfaces/index.ts
@@ -1,11 +1,29 @@
+import themes from '@plurid/plurid-themes';
+
+
+
 export interface ChiselNode {
     text: string;
 }
+
 
 export interface ChiselValue {
     nodes: ChiselNode[]
 }
 
+
 export interface ChiselProperties {
     value: ChiselValue;
+    configuration: ChiselConfiguration;
+}
+
+
+export interface ChiselConfiguration {
+    theme: keyof typeof themes | ChiselConfigurationTheme;
+}
+
+
+export interface ChiselConfigurationTheme {
+    general: keyof typeof themes;
+    interaction: keyof typeof themes;
 }
