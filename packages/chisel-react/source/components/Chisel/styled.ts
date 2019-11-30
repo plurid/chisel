5c22a50c131b6a9312b08e9908d1c2c83205790b
Jan Cavel <caveljan@gmail.com>
2019-11-30 16:06:45 +0200

    chisel-react > dev: Chisel

--- packages/chisel-react/source/components/Chisel/styled.ts
+++ packages/chisel-react/source/components/Chisel/styled.ts
@@ -6,4 +6,6 @@ export const StyledChisel = styled.div`
     background-color: ${props => props.theme.backgroundColorPrimary};
     color: ${props => props.theme.colorPrimary};
     font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    font-size: 0.9rem;
+    padding: 1rem;
 `;
