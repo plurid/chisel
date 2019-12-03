c14b2917cca55a01bd711f645f86977cdfe3e90b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-12-03 20:41:15 +0200

    chisel-react > dev: utilities

--- packages/chisel-react/source/utilities/index.ts
+++ packages/chisel-react/source/utilities/index.ts
@@ -1,39 +1,43 @@
 export const getSelectionCaretAndLine = (editable: HTMLDivElement | null) => {
-    if (!editable) return;
+    try {
+        if (!editable) return;
 
-    // collapse selection to end
-    (window as any).getSelection().collapseToEnd();
+        // collapse selection to end
+        (window as any).getSelection().collapseToEnd();
 
-    const sel = window.getSelection();
-    if (!sel) return;
-    const range = sel.getRangeAt(0);
+        const sel = window.getSelection();
+        if (!sel) return;
+        const range = sel.getRangeAt(0);
 
-    // get anchor node if startContainer parent is editable
-    let selectedNode = editable === range.startContainer.parentNode
-      ? sel.anchorNode
-      : range.startContainer.parentNode;
+        // get anchor node if startContainer parent is editable
+        let selectedNode = editable === range.startContainer.parentNode
+          ? sel.anchorNode
+          : range.startContainer.parentNode;
 
-    if (!selectedNode) {
+        if (!selectedNode) {
+            return {
+                caret: -1,
+                line: -1,
+            };
+        }
+
+        // select to top of editable
+        range.setStart((editable as any).firstChild, 0);
+
+        // do not use 'this' sel anymore since the selection has changed
+        const content = (window as any).getSelection().toString();
+        const text = JSON.stringify(content);
+        const lines = (text.match(/\\n/g) || []).length + 1;
+
+        // clear selection
+        (window as any).getSelection().collapseToEnd();
+
+        // minus 2 because of strange text formatting
         return {
-            caret: -1,
-            line: -1,
-        };
-    }
-
-    // select to top of editable
-    range.setStart((editable as any).firstChild, 0);
-
-    // do not use 'this' sel anymore since the selection has changed
-    const content = (window as any).getSelection().toString();
-    const text = JSON.stringify(content);
-    const lines = (text.match(/\\n/g) || []).length + 1;
-
-    // clear selection
-    (window as any).getSelection().collapseToEnd();
-
-    // minus 2 because of strange text formatting
-    return {
-        caret: text.length - 2,
-        line: lines,
+            caret: text.length - 2,
+            line: lines,
+        }
+    } catch (error) {
+        return;
     }
 }
