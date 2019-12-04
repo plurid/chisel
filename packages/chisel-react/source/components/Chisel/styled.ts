f4916be5ccba7866ffa3c6b0c28d9a8985465e1a
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-12-04 10:46:05 +0200

    chisel-react > dev: Chisel, styling

--- packages/chisel-react/source/components/Chisel/styled.ts
+++ packages/chisel-react/source/components/Chisel/styled.ts
@@ -6,11 +6,12 @@ export const StyledChisel = styled.div`
     background-color: ${props => props.theme.backgroundColorPrimary};
     color: ${props => props.theme.colorPrimary};
 
-    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-    font-size: 0.9rem;
+    width: 100%;
+    height: 100%;
     padding: 1rem;
-
     outline: none;
+    font-family: 'Ubuntu', 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    font-size: 0.9rem;
     white-space: pre-wrap;
     word-break: break-word;
 `;
