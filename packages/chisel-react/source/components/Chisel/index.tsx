78336dd779c5b9f4fd8d3c3e46f4d1e76fb340de
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-12-31 21:49:52 +0200

    chisel-react > dev: Chisel | PieceTable implementation

--- packages/chisel-react/source/components/Chisel/index.tsx
+++ packages/chisel-react/source/components/Chisel/index.tsx
@@ -4,6 +4,10 @@ import React, {
     useEffect,
 } from 'react';
 
+import {
+    PieceTable,
+} from '@plurid/chisel';
+
 import themes from '@plurid/plurid-themes';
 
 import {
@@ -28,6 +32,8 @@ const emptyEditorValue: ChiselValue = {
 
 const Chisel: React.FC<ChiselProperties> = (properties) => {
     const editor = useRef<HTMLDivElement>(null);
+    const pieceTable = useRef(new PieceTable(''));
+    const cursor = useRef(0);
 
     const {
         value,
@@ -40,66 +46,107 @@ const Chisel: React.FC<ChiselProperties> = (properties) => {
         nodes,
     } = value;
 
+    const [text, setText] = useState('');
+
     const [editorValue, setEditorValue] = useState<ChiselValue>({...emptyEditorValue});
     const [theme, seTheme] = useState(themes.plurid);
 
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    const handleKeyDown = (
+        event: React.KeyboardEvent<HTMLDivElement>,
+    ) => {
+        event.preventDefault();
+
+        const printableKey = event.key.length === 1;
+
+        if (printableKey) {
+            pieceTable.current.insert(event.key, cursor.current);
+            cursor.current += 1;
+            setText(pieceTable.current.getSequence());
+        }
+
+        if (event.key === 'Backspace') {
+            if (cursor.current > 0) {
+                pieceTable.current.delete(cursor.current - 1, 1);
+                cursor.current -= 1;
+                setText(pieceTable.current.getSequence());
+            }
+        }
+
+        if (event.key === 'Enter') {
+            pieceTable.current.insert('\n', cursor.current);
+            cursor.current += 1;
+            setText(pieceTable.current.getSequence());
+        }
+
+        // console.log(cursor.current);
+        // console.log(editor.current?.childNodes[0]);
+
+        // const range = document.createRange();
+        // const selection = window.getSelection();
+        // range.setStart(editor.current!.childNodes[0], cursor.current);
+        // range.collapse(true);
+        // selection!.removeAllRanges();
+        // selection!.addRange(range);
+
+
         // if (event.key === 'Tab') {
         //     event.preventDefault();
         // }
-        if (event.key === 'Enter') {
-            // event.preventDefault();
-            window.document.execCommand('insertHTML', false, '\n');
-        }
+        // if (event.key === 'Enter') {
+        //     // event.preventDefault();
+        //     window.document.execCommand('insertHTML', false, '\n');
+        // }
     }
 
     const handleKeyUp = (
         event: React.KeyboardEvent<HTMLDivElement>,
     ) => {
-        const value = editor.current!.innerText;
-        console.log(editor.current!.innerText);
-        console.log('----');
-        console.log(editor.current!.textContent);
-        console.log('----');
-        console.log(editor.current!.innerHTML);
-        console.log('----');
+        // const value = editor.current!.innerText;
+        // console.log(editor.current!.innerText);
+        // console.log('----');
+        // console.log(editor.current!.textContent);
+        // console.log('----');
+        // console.log(editor.current!.innerHTML);
+        // console.log('----');
 
-        const splitValue = value
-            .split(/\n/);
+        // const splitValue = value
+        //     .split(/\n/);
 
-        const editorNodes = splitValue.map(nodeText => {
-            const node: ChiselNode = {
-                type: 'paragraph',
-                text: nodeText,
-            };
-            return node;
-        });
-        const editorValue: ChiselValue = {
-            nodes: editorNodes,
-        };
+        // const editorNodes = splitValue.map(nodeText => {
+        //     const node: ChiselNode = {
+        //         type: 'paragraph',
+        //         text: nodeText,
+        //     };
+        //     return node;
+        // });
+        // const editorValue: ChiselValue = {
+        //     nodes: editorNodes,
+        // };
 
-        atChange(
-            editorValue,
-            event,
-        );
+        // atChange(
+        //     editorValue,
+        //     event,
+        // );
     }
 
-    const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const handleClick = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    ) => {
         // const selection = getSelectionCaretAndLine(editor.current);
         // if (selection) {
         //     // console.log('selection', selection);
         // }
     }
 
-    useEffect(() => {
-        if (nodes) {
-            setEditorValue({
-                nodes,
-            });
-        }
-    }, [
-        nodes,
-    ]);
+    // useEffect(() => {
+    //     if (nodes) {
+    //         setEditorValue({
+    //             nodes,
+    //         });
+    //     }
+    // }, [
+    //     nodes,
+    // ]);
 
     useEffect(() => {
         if (configuration) {
@@ -129,7 +176,8 @@ const Chisel: React.FC<ChiselProperties> = (properties) => {
             ref={editor}
             style={{...style}}
         >
-            {editorValue.nodes.map(node => {
+            {text}
+            {/* {editorValue.nodes.map(node => {
                 const {
                     text,
                 } = node;
@@ -144,7 +192,7 @@ const Chisel: React.FC<ChiselProperties> = (properties) => {
                         {text}
                     </div>
                 );
-            })}
+            })} */}
         </StyledChisel>
     );
 };
