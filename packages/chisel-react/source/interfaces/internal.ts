115b1f65eac47b1ee2f4e58b4f8116f1c706ba63
Jan Cavel <caveljan@gmail.com>
2020-01-16 14:13:53 +0200

    chisel-react > dev: Chisel, cursors

--- packages/chisel-react/source/interfaces/internal.ts
+++ packages/chisel-react/source/interfaces/internal.ts
@@ -2,9 +2,11 @@ export interface Cursor {
     index: number;
 }
 
+
 export interface InternalCursor extends Cursor {
     editor: boolean;
 }
+
 
 export interface ExternalCursor extends Cursor {
     owner: string;
